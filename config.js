0a558dd66bee8edde0a57f73590370b432c05114
roshiroku <shir.oraan@gmail.com>
2024-10-06 01:08:30 +0300

    update seeding

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 import config from "config";
 
-export const env = config.get("env");
 export const db = config.get("db");
 export const auth = config.get("auth");
 export const validation = config.get("validation");
 export const logging = config.get("logging");
 export const cors = config.get("cors");
-export const users = config.has("defaultData.users") ? config.get("defaultData.users") : [];
-export const cards = config.has("defaultData.cards") ? config.get("defaultData.cards") : [];
--- seed.js
+++ seed.js
@@ -1,10 +1,19 @@
-import { users as defaultUsers, cards as defaultCards } from "./config.js";
+import fs from "fs/promises";
+import path from "path";
 import { error, info, success } from "./logging/loggingProvider.js";
 import Card from "./models/Card.js";
 import User from "./models/User.js";
 
+const USERS_PATH = path.resolve("./seed/users.json");
+const CARDS_PATH = path.resolve("./seed/cards.json");
+
 export default async function seedDatabase() {
   try {
+    const usersFile = await fs.readFile(USERS_PATH, "utf-8");
+    const cardsFile = await fs.readFile(CARDS_PATH, "utf-8");
+    const defaultUsers = JSON.parse(usersFile);
+    const defaultCards = JSON.parse(cardsFile);
+
     if (!defaultUsers?.length) return;
 
     const existingUsers = await User.find();
@@ -16,11 +25,12 @@ export default async function seedDatabase() {
     await Promise.all(defaultUsers.map(User.register));
     success("Users seeded successfully.");
 
-    const [businessUser] = await User.find({ isBusiness: true });
+    const businessUsers = await User.find({ isBusiness: true });
 
-    if (defaultCards?.length && businessUser) {
+    if (defaultCards?.length && businessUsers.length) {
       await Promise.all(defaultCards.map(card => {
-        return Card.add({ ...card, user_id: businessUser._id });
+        const user_id = businessUsers[Math.floor(Math.random() * businessUsers.length)]._id;
+        return Card.add({ ...card, user_id });
       }));
       success("Cards seeded successfully.");
     }
