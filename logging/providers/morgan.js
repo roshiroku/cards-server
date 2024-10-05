1e16c411f37a5b0ca2df3f0e69a6e4750d847127
roshiroku <shir.oraan@gmail.com>
2024-10-05 18:27:00 +0300

    add logging format and colors config

--- logging/providers/morgan.js
+++ logging/providers/morgan.js
@@ -1,20 +1,24 @@
 import morgan from "morgan";
 import { now } from "../../utils/timeUtils.js";
 import chalk from "chalk";
+import { logging } from "../../config.js";
 
-const success = chalk.cyanBright;
-const error = chalk.redBright;
+const success = chalk[logging.colors?.success] ?? chalk.cyanBright;
+const error = chalk[logging.colors?.error] ?? chalk.redBright;
 
-export default morgan((tokens, req, res) => {
-  const { date, time } = now();
-  const message = [
-    `[${date} ${time}]`,
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    "-",
-    `${tokens["response-time"](req, res)}ms`,
-  ].join(" ");
+export default morgan(logging.format == "custom" ? (
+  (tokens, req, res) => {
+    const { date, time } = now();
+    const message = [
+      `[${date} ${time}]`,
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      "-",
+      `${tokens["response-time"](req, res)}ms`,
+    ].join(" ");
 
-  return res.statusCode < 400 ? success(message) : error(message);
-});
+    return res.statusCode < 400 ? success(message) : error(message);
+  }) : (
+  logging.format
+));
