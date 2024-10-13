274538a3a7c7900844fd990be75bb98888e7536d
roshiroku <shir.oraan@gmail.com>
2024-10-14 00:37:54 +0300

    update static folder path

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors);
 app.use(logging);
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 app.use("/users", usersController);
 app.use("/cards", cardsController);
